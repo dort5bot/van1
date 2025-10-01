928845bbf28982c1c1fad511328976e2eadb62d3
dort5bot <dersdep@gmail.com>
2025-10-01 08:20:13 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,20 @@
+# bot/config.py
+
+class BinanceAPI:
+    BASE_URL = "https://api.binance.com"
+    FUTURE_URL = "https://fapi.binance.com"
+    TIMEOUT = 10
+
+    class Public:
+        class Spot:
+            TICKER = "/api/v3/ticker/price"
+            ORDERBOOK = "/api/v3/depth"
+
+        class Future:
+            TICKER = "/fapi/v1/ticker/price"
+            FUNDING_RATE = "/fapi/v1/fundingRate"
+
+
+# diğer genel ayarların varsa onlar da aynı dosyada olur:
+BOT_NAME = "MyTelegramBot"
+DEBUG = True
